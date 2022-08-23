14abc08f389b65513269531f21f427b969c9abf4
tamim-i-chowdhury <tamim.c183@gmail.com>
2022-08-23 22:19:48 +0600

    everything up to date now

--- js/common.js
+++ js/common.js
@@ -2,6 +2,11 @@ function getElementValue(elementId) {
   const element = document.getElementById(elementId);
   const perElementSalaryString = element.value;
   const perElementSalaryAmount = parseFloat(perElementSalaryString);
+  if (isNaN(perElementSalaryAmount)) {
+    alert("Please provide a valid number.");
+    element.value = "";
+    return;
+  }
   return perElementSalaryAmount;
 }
 function displayAmount(elementId, value) {
